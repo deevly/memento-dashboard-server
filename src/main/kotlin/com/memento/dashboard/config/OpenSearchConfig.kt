c1abe09e95f3ed4a76919ee7149a509ce1db00e8
ReasonH <azsxd34@naver.com>
2022-06-12 23:31:53 +0900

    Auth Config

--- src/main/kotlin/com/memento/dashboard/config/OpenSearchConfig.kt
+++ src/main/kotlin/com/memento/dashboard/config/OpenSearchConfig.kt
@@ -1,22 +1,61 @@
 package com.memento.dashboard.config
 
+import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.client.CredentialsProvider
+import org.apache.http.conn.ssl.NoopHostnameVerifier
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.ssl.SSLContextBuilder
+import org.apache.http.ssl.SSLContexts
+import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+
 
 @Configuration
 class OpenSearchConfig {
 
-//    @Bean
-//    fun client(): RestHighLevelClient {
-//        val configuration = ClientConfiguration
-//            .builder()
-//            .connectedTo("15.164.96.210:9200")
-//            .build()
-//
-//        return RestClients.create(configuration).rest()
-//    }
-//
-//    @Bean
-//    fun elasticsearchTemplate(): ElasticsearchOperations {
-//        return ElasticsearchRestTemplate(client())
-//    }
+    @Bean
+    fun client(): RestHighLevelClient {
+        return try {
+
+            val credentialsProvider: CredentialsProvider = BasicCredentialsProvider()
+            credentialsProvider.setCredentials(AuthScope.ANY, UsernamePasswordCredentials("admin", "1234"))
+
+            val sslBuilder: SSLContextBuilder = SSLContexts.custom()
+                .loadTrustMaterial(
+                    null
+                ) { x509Certificates: Array<X509Certificate?>?, s: String? -> true }
+            val sslContext: SSLContext = sslBuilder.build()
+            val client = RestHighLevelClient(RestClient
+                .builder(HttpHost("13.125.194.56", 9200, "https"))
+                .setHttpClientConfigCallback { httpClientBuilder ->
+                    httpClientBuilder
+                        .setDefaultCredentialsProvider(credentialsProvider)
+                        .setSSLContext(sslContext)
+                        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                }
+                .setRequestConfigCallback { requestConfigBuilder ->
+                    requestConfigBuilder.setConnectTimeout(5000)
+                        .setSocketTimeout(120000)
+                }
+            )
+            println("elasticsearch client created")
+            client
+        } catch (e: Exception) {
+            println(e)
+            throw Exception("Could not create an elasticsearch client!!")
+        }
+    }
+
+    @Bean
+    fun elasticsearchTemplate(): ElasticsearchOperations {
+        return ElasticsearchRestTemplate(client())
+    }
 }
